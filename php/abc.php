bc3457c9a3e8fb1fd21c11e0feb7fe981fabbadf
alamin <amin.se.aiub@gmail.com>
2023-06-13 15:58:45 +0600

    intro php

---
+++ php/abc.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>TEST PHP</title>
+</head>
+<body>
+        <?php for($i=0; $i<4; $i++){ ?>
+
+            <h1>TEST</h1>    
+        
+        <?php  } ?>
+        
+        
+</body>
+</html>
---
+++ php/test.php
@@ -0,0 +1,55 @@
+<?php 
+
+    $name = "alamin";
+    $id = 12;
+    $cgpa = 2.5;
+    $student = [12, 'alamin', 2.5]; 
+    // $students = [
+    //                 [12, 'alamin', 2.5], 
+    //                 [12, 'alamin', 2.5], 
+    //                 [12, 'alamin', 2.5] 
+    //             ];
+    // $student[2][2];
+
+    // $student = ['id'=>12, 'name'=>'alamin', 'cgpa'=>2.5]; 
+    // $students = [
+    //                 's1'=>['id'=>12, 'name'=>'alamin', 'cgpa'=>2.5], 
+    //                 's2'=>['id'=>12, 'name'=>'alamin', 'cgpa'=>2.5], 
+    //                 's3'=>['id'=>12, 'name'=>'alamin', 'cgpa'=>2.5]
+    //             ];
+
+    // $students['s3']['cgpa'];
+
+    // if(){
+
+    // }else{
+
+    // }
+
+    for($i=0; $i<10; $i++){
+        echo "<h1>Your name: Al-Amin </h1>";
+    }
+
+    // foreach($student as $s){
+    //     echo $s;
+    // }
+
+    //print_r($student);
+
+    // function abc($id=0, $name=""){
+
+    //     return $abc;
+    // }
+
+    //abc(1);
+
+    //print('hello PHP');
+    //echo "\r\n".$id;
+
+    // if(0 === "0"){
+    //     echo "T";
+    // }else{
+    //     echo "F";
+    // }
+    
+?>
