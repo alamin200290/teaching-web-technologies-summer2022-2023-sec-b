3fd70477bf8e03ba8c6c8e217955bab3093d5900
alamin <amin.se.aiub@gmail.com>
2023-07-23 15:50:43 +0600

    into JS

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+//name = 'alamin';
+//var name = 'alamin';
+let name = 'alamin';
+const id = 12;
+let student = ['12', 34, 3.4];
+// function abc(){
+
+// }
+//let f1 = (a, b)=>a+b;
+
+console.log('test');
+function test(){
+    alert('test');
+}
+
+// document.write('<h1>Another test</h1>');
+// document.getElementsByTagName('h1')[0].innerHTML = 'Only JS test';
+// document.getElementsByTagName('h1')[1].style.color = 'red';
+// document.getElementById('h1')[0];
+
+function checkUser(){
+    let username = document.getElementById('username').value;
+    //alert(username);
+    document.getElementById('abc').innerHTML = username;
+}
